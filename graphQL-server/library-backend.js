5b5e658147c91fee7715c877455ddc29d56afff2
Luke <nycthbre@gmail.com>
2024-09-03 20:35:57 +0200

    Exercise 8.3 completion (graphql)

--- graphQL-server/library-backend.js
+++ graphQL-server/library-backend.js
@@ -102,12 +102,17 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
   type Book {
     title: String!
     author: String!
     published: Int!
     genres: [String!]!
+  }
+  type Author {
+    name: String!
+    bookCount: Int!
   }
 `;
 
@@ -116,6 +121,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length,
   },
 };
 
